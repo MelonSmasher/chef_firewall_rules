8717da2f245065f38be986630da9748c6ca80fb0
Alex Markessinis <markea@sage.edu>
2017-03-17 12:10:54 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# firewall_rules
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: firewall_rules
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
