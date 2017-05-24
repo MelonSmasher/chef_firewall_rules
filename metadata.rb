cd81fd701e7ddfe887de27485de0b5c1ef680a97
Alex Markessinis <markea@sage.edu>
2017-05-24 09:49:19 -0400

    updated firewall cookbook dependancy

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'MIT'
 description 'Configures firewall rules through attributes.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version '0.1.7'
+version '0.2.0'
 
 supports 'amazon'
 supports 'centos'
@@ -17,8 +17,8 @@ supports 'scientific'
 supports 'ubuntu'
 supports 'windows'
 
-depends 'firewall', '~> 2.5.4'
-chef_version '>= 12.4' if respond_to?(:chef_version)
+depends 'firewall', '>= 2.6.1'
+chef_version '>= 12.1' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/MelonSmasher/chef_firewall_rules/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/MelonSmasher/chef_firewall_rules' if respond_to?(:source_url)
